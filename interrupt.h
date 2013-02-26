9938fa878e62ab43663f366627bb4d9c0ae68f5c
David DiPaola <dsd3275@rit.edu>
2013-02-26 12:27:22 -0500

    Marged in Xinu interrupt routine resources

--- interrupt.h
+++ interrupt.h
@@ -1,6 +1,15 @@
-#ifndef __INTERRUPT_H
-#define __INTERRUPT_H
+/**
+ * @file interrupt.h
+ * 
+ * Constants and declarations associated with interrupt and exception
+ * processing.
+ *
+ */
 
+#ifndef _INTERRUPT_H_
+#define _INTERRUPT_H_
+
+#include "stdint.h"
 #include "io.h"
 
 #define INTERRUPT_BASE_ADDR 0x2000B000
@@ -31,14 +40,35 @@
 #define INTERRUPT_DISABLEIRQ2     __IO(INTERRUPT_DISABLEIRQ2_ADDR)
 #define INTERRUPT_DISABLEBASICIRQ __IO(INTERRUPT_DISABLEBASICIRQ_ADDR)
 
-#endif
+typedef void (__attribute__((interrupt("IRQ"))) *irq_handler)( void );
 
 /*
-Copyright (c) 2013 David DiPaola
+ * irqmasks are laid out like the following:
+ * 
+ * bits 0-15: whether a given interrupt is enabled or not.
+ * bit 16: whether interrupts are enabled (1) or disabled (0)
+ *
+ * The reason for this is that restore() is responsible
+ *  for enabling/disabling interrupts as well as restoring
+ *  the IRQ mask. 
+ *
+ * See Comer, "OS Design: The XINU approach, Linksys edition," pg. 40.
+ */
+//typedef unsigned long irqmask;  /**< machine status for disable/restore  */
+typedef struct {
+    uint32_t lower;
+    uint32_t upper;
+} irqmask;
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+/* Interrupt enabling function prototypes */
+irqmask disable(void);
+irqmask restore(irqmask);
+irqmask enable(void);
+irqmask enable_irq( int irq );
+irqmask disable_irq( int irq );
+void register_irq( int irq, irq_handler handler );
+irq_handler get_irq(int irq);
+void irq_handled( void );
+int in_interrupt( void );
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+#endif                          /* _INTERRUPT_H_ */
