cd8320c831c0017584d172cd17c2aee39defa0f4
rememberthe8bit <dsd3275@rit.edu>
2013-02-26 12:41:17 -0500

    Cleaned up code. Re-enabled the enable-disable test.

--- blinker07.c
+++ blinker07.c
@@ -45,107 +45,31 @@ int notmain ( void )
     vic_init();
 
     //register handlers and enable IRQs
-    /*
-    vic_register_irq(VECT_SYSTIMERM1, systimer_handler);
-    vic_register_irq(VECT_PL011, uart_handler);
-    vic_enable(VECT_PL011);
-    vic_enable(VECT_SYSTIMERM1);
-    */
-    
     register_irq(VECT_SYSTIMERM1, systimer_handler);
     register_irq(VECT_PL011, uart_handler);
     enable_irq(VECT_PL011);
     enable_irq(VECT_SYSTIMERM1);
-    
-    enable_cpu_irq();
-    
 
     //initialize hardware
     systimer_init(1000000); //go off once a second
     uartInit();
     uartPutln("Booted!");
-    //uartPut32(0x12345678);
-
-    //test the UART's interrupt bit
-    /*
-    iuartInit();
-    GPIOSET(16); //led off
-    PL011_DR = '0'; //fill the transmit FIFO
-    PL011_DR = '1';
-    PL011_DR = '2';
-    PL011_DR = '3';
-    PL011_DR = '4';
-    PL011_DR = '5';
-    PL011_DR = '6';
-    PL011_DR = '7';
-    PL011_DR = '8'; //characters past (and including) this one aren't printed...
-    PL011_DR = '9';
-    PL011_DR = 'A';
-    PL011_DR = 'B';
-    PL011_DR = 'C';
-    PL011_DR = 'D';
-    PL011_DR = 'E';
-    PL011_DR = 'F';
-    ra = INTERRUPT_IRQPEND2; //get the upper 32bits of IRQ pending
-    while((rb = INTERRUPT_IRQPEND2) == ra){} //wait for IRQ pending to change
-    rc = PL011_RIS; //get raw interrupt status
-    uartPutln("");
-    uartPut32(ra); //print the old value of IRQ pending
-    uartPut32(rb); //print the new value
-    uartPut32(rc); //print the PL011 interrupt status
-    for(;;){} //chill
-    */
-
-    //test the UART with an interrupt
-    /*
-    GPIOSET(16); //led off
-    iuartInit();
-    enable_irq();
-    PL011_DR = '1'; //fill the transmit FIFO
-    PL011_DR = '2';
-    PL011_DR = '3';
-    PL011_DR = '4';
-    PL011_DR = '5';
-    PL011_DR = '6';
-    PL011_DR = '7';
-    PL011_DR = '8';
-    for(;;){} //only do interrupts
-    */
 
     //test interrupts fully
     GPIOSET(16); //led off
+    enable_cpu_irq();
     //enable(); //wish this worked...
     iuartInit();
-    //PL011_DR = '?';
     for(;;){
         iuartPutln("Interrupts!");
-        //vic_enable_irq(VECT_SYSTIMERM1);
-        //vic_disable_irq(VECT_SYSTIMERM1); //test deregistration. this line prevents the LED from blinking.
+        enable_irq(VECT_SYSTIMERM1);
+        disable_irq(VECT_SYSTIMERM1);
         if(ra != (rb = systimer_get()))
         {
             ra = rb;
             led_toggle();
         }
     }
-
-    //test system timer AND UART
-    /*
-    while(1)
-    {
-        iuartPutln("Interrupts!");
-    }*/
-    /*
-        while(systimer_get() == ra) continue;
-        ra = systimer_get();
-
-        GPIOCLR(16); //LED on
-
-        while(systimer_get() == ra) continue;
-        ra = systimer_get();
-
-        GPIOSET(16); //LED off
-    }*/
-
 
     return(0);
 }
@@ -155,7 +79,7 @@ int notmain ( void )
 
 //-------------------------------------------------------------------------
 //
-// Copyright (c) 2012 David Welch dwelch@dwelch.com
+// Copyright (c) 2012 David Welch dwelch@dwelch.com, 2013 David DiPaola dsd3275@rit.edu
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 //
